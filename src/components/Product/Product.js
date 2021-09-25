0fb905876b1b40f762524b1c0a92afbb6955d6d9
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-09-25 14:38:18 +0600

    search product applied

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const {img,name,seller,price,stock} = props.product;
+    const {img,name,seller,price,stock,star} = props.product;
     return (
         <div className="single-product">
             <div className="product-img">
@@ -14,6 +15,13 @@ const Product = (props) => {
                 <h4 className="product-name">{name}</h4>
                 <p className="product-seller">By: {seller}</p>
                 <p className="product-price">${price}</p>
+                <Rating 
+                readonly
+                initialRating={star}
+                emptySymbol="far fa-star icon-color"
+                fullSymbol="fas fa-star icon-color"
+                ></Rating>
+                <br />
                 <p className="product-stock">Only {stock} left in stock - order soon</p>
                 <button className="add-button" onClick={ ()=> props.handleAddProduct(props.product) }> <FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
             </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -11,16 +11,22 @@ import {addToDB,getLocalValue} from '../../utilities/addToLocal';
 const Shop = () => {
     const firs10 = fakeData.slice(0,10);
     const [products,setProducts] = useState(firs10);
+    const [searchProducts,setSearchProducts] = useState(firs10);
     const [cart,setCart] = useState(getLocalValue(fakeData));
     const handleAddProduct = (product)=> {
         addToDB(product.key);
         setCart(getLocalValue(fakeData));
     }
+    const handleSearch = event=> {
+        const searchText = event.target.value;
+        const matched = fakeData.filter(dt=> ((dt.name).toLowerCase()).includes(searchText.toLowerCase()));
+        setSearchProducts(matched);   
+    }
     return (
         
         <div>
             <nav className="search">
-                <input className="search-input" type="text" placeholder="Search product" />
+                <input className="search-input" onChange={handleSearch} type="text" placeholder="Search product" />
                 <FontAwesomeIcon icon={faShoppingCart} className="cart-icon"/>
                 <p className="cart-number">{cart.length}</p>
             </nav>
@@ -28,7 +34,7 @@ const Shop = () => {
                 <div className="product-container">
                     <ul>
                         {
-                            products.map(pd => <Product key={pd.key} product={pd} handleAddProduct = {handleAddProduct}></Product> )
+                            searchProducts.map(pd => <Product key={pd.key} product={pd} handleAddProduct = {handleAddProduct}></Product> )
                         }
                     </ul>
                 </div>
